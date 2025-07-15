14c0c4ad71ef9554228adadab17716aa289de3d5
TJK98 <wtj1998@naver.com>
2025-07-15 15:02:27 +0900

    add MemberController3_3 with basic CRUD operations and response handling

---
+++ src/main/java/com/spring/basic/chap3_4/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.spring.basic.chap3_4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 전역 크로스 오리진 설정 : 특정 클라이언트가 API를 사용하기 위한 정책 설정
+// 같은 도메인이면 CORS 설정은 안 해줘도 된다.
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                // 요청을 허용할 서버의 URL
+                .addMapping("/api/v3-3/*") // *은 한 단계, **은 끝까지 다
+                // 어떤 클라이언트를 허용할 것인지
+                .allowedOrigins("http://127.0.0.1:5500")
+                // 어떤 요청 방식을 허용할지
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+        ;
+    }
+}
