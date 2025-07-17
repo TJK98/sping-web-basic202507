928d4a9c77e5f45cbebd26a1d4e4fde2818d33fd
TJK98 <wtj1998@naver.com>
2025-07-17 11:18:06 +0900

    add ScoreApiController with CRUD operations, DTO integration, and input validation for score management alongside new DTOs and utility methods

--- src/main/java/com/spring/basic/score/entity/Score.java
+++ src/main/java/com/spring/basic/score/entity/Score.java
@@ -20,6 +20,7 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Score {
     private Long id; // 학번
     private String name; // 이름
@@ -28,4 +29,20 @@ public class Score {
     private int math; // 수학 점수
     private int total; // 총점
     private double average; // 평균
+    private int rank; // 성적 순위
+
+    public Score(Long id, String name, int kor, int eng, int math) {
+        this.id = id;
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+        calcTotalAndAverage();
+    }
+
+    // 총점 평균을 구하는 메서드
+    public void calcTotalAndAverage() {
+        this.total = this.kor + this.eng + this.math;
+        this.average = this.total / 3.0;
+    }
 }
